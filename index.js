d0dbdb50d84db001696c633e34bd1ef1bfa60fb0
jobayercse71 <jobayercse71.com>
2021-10-31 02:10:44 +0600

    port update

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const cors = require('cors');
 const app = express()
 const ObjectId = require('mongodb').ObjectId;
-const port = porcess.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
